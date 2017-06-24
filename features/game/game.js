dcdf6015312ff6c145360c3f50cdeb6f13ee8b64
Micah Isley <micahisley@Micahs-MacBook-Air.local>
2017-06-24 12:39:20 -0400

    some games updates

--- features/game/game.js
+++ features/game/game.js
@@ -2,6 +2,10 @@ var damageDealt;
 var heroSpeed;
 var enemySpeed;
 var currentEnemy;
+var number;
+var typeOfEnemy;
+var currentHero = fenla;
+var wolfForm = false;
 
 function Hero(race, role, hp, strength, constitution, dexterity, intelligence, wisdom, charisma){
 	this.race = race;
@@ -36,6 +40,8 @@ Hero.prototype.speed = function() {
 	}
 }
 
+
+
 Enemy.prototype.attack = function() {
 	damageDealt = Math.ceil(Math.random() * this.strength);
 	console.log("Enemy Dmg: " + damageDealt);
@@ -43,6 +49,18 @@ Enemy.prototype.attack = function() {
 }
 
 var fenla = new Hero("gnome", "druid", 30, 10, 13, 15, 12, 16, 9);
+	fenla.fireball = function(){
+		damageDealt = ((Math.ceil(Math.random() * this.wisdom)) + 10);
+		for(i = 0; i < 3; i++){
+			number = parseInt(i);
+			console.log(number);
+			console.log(typeOfEnemy[i]);
+			typeOfEnemy[i].hp -= damageDealt;
+			$("#"+i).find(".hpDisplay").text("Hp: " + typeOfEnemy[i].hp);
+			checkMassDeath(typeOfEnemy[i], i);
+		}
+
+	}
 var moro = new Hero("human", "monk", 28, 8, 14, 16, 10, 11, 13);
 var goblin = new Enemy("goblin", 14, 6);
 var ogre = new Enemy("ogre", 15, 11);
@@ -58,34 +76,38 @@ function combat(hero, enemy){
 				hero.attack();
 				enemy.hp -= damageDealt;
 				enemyLoseLife();
-				$("#enemyHp").text("Hp: " + enemy.hp);
-				checkDeath(hero, enemy);
+				$("#"+number).find(".hpDisplay").text("Hp: " + enemy.hp);
+				checkIndividualDeath(hero, enemy);
 				setTimeout(function(){
 					heroAttack(x);
 				}, 500);	
 			}
 		}
 		heroAttack(0);
-		if(enemy.hp > 0){
-			setTimeout(function(){	
-				enemy.attack();
-				hero.hp -= damageDealt;
-				heroLoseLife();
-				$("#heroHp").text("Hp: " + hero.hp);
-				checkDeath(hero, enemy);
-			}, 1000);
-			$("#heroHp").text("Hp: " + hero.hp);
-			$(".enemyHp").text("Hp: " + enemy.hp);
-		}	
 	}else{}
 }
 
-function checkDeath(hero, enemy){
-	if(hero.hp <= 0 && enemy.hp >0){
+function checkIndividualDeath(hero, enemy){
+	console.log("hey");
+	if(enemy.hp <= 0){
+		$("#"+number).find(".hpDisplay").text("Hp: 0");
+		setTimeout(function(){
+			$("#"+number).remove();
+		},1000);
+	}else if(hero.hp <= 0 && enemy.hp >0){
 		console.log("You Lose...");
 	}else if (hero.hp > 0 && enemy.hp <=0){
 		console.log("You Win!!");
 	}else {}
+}
+
+function checkMassDeath(enemy, i){
+	if(enemy.hp <= 0) {
+		$("#"+i).find(".hpDisplay").text("Hp: 0");
+		setTimeout(function(){
+			$("#"+i).remove();
+		},1000);
+	}
 }
 
 
@@ -106,15 +128,17 @@ $("#attackButton").click(function(){
 })
 
 $(document).on("click", ".generated", function(){
-	currentEnemy = this.id;
-	combat(fenla, currentEnemy);
-	console.log(currentEnemy);
+	number = parseInt(this.id);
+	combat(fenla, typeOfEnemy[number]);
+	setTimeout(function(){
+		enemyCombat(fenla, typeOfEnemy[number]);
+	}, 1000);	
 })
 
 function enemyLoseLife(){
 	$(".damageTaken").empty();
 	var damageNumber = $("<div class='damageAnimate'>" + damageDealt + "</div>");
-	$("#enemy").find(".damageTaken").append(damageNumber);
+	$("#"+number).find(".damageTaken").append(damageNumber);
 	damageNumber.animate({ top: "-=15"}, "slow").delay(50).fadeOut(100);
 }
 
@@ -147,22 +171,45 @@ $(document).keydown(function(e) {
 });
 
 function summonEnemies(name, number){
+	typeOfEnemy = name;
 	for(i=0; i<number; i++){
 		randHp = ((Math.floor(Math.random() * name.hp)) + 10);
 		name[i] = new Enemy(name.name, randHp, name.strength);
 		console.log(name[i]);
-		var newObject = $("<div>").attr("class", "generated").attr("id", name.name+"["+i+"]");
+		var newObject = $("<div>").attr("class", "generated").attr("id", i);
 		console.log(newObject);
 		var statBox = $("#enemy").append(newObject);
-		$("#"+name.name+i).html("<span>Hp: "+name[i].hp+"</span>");
-		generateEnemyButtons();
+		var enemyName = name.name.toUpperCase();
+		$("#"+i).html("<div class='enemyName'>"+enemyName+"</div>").append("<span class='hpDisplay'>Hp: "+name[i].hp+"</span>").append("<span class='damageTaken'></span>");
 	}
 
 }
 
-function generateEnemyButtons(){
-	$("#enemyButtonArea").append("<div class='enemyButtons'>"+i);
+function doSetTimeout(i) {
+  setTimeout(function() { 
+  	typeOfEnemy[i].attack();
+  	console.log(fenla.hp);
+	fenla.hp -= damageDealt;
+	heroLoseLife();
+	$("#heroHp").text("Hp: " + fenla.hp);
+	checkIndividualDeath(hero, enemy); }, i * 2000);
 }
 
-summonEnemies(ogre, 4);
-console.log(ogre[1-1]);
+function enemyCombat(hero, enemy){
+	//if(enemy.hp > 0){
+		var enemyLength = $("#enemy > div").length;
+		for(i=0; i<enemyLength; i++){
+			doSetTimeout(i);
+
+		}	
+		$("#heroHp").text("Hp: " + hero.hp);
+		$("#"+number).find(".hpDisplay").text("Hp: " + enemy.hp);
+	//}
+}
+
+$("#specialButton").click(function(){
+	fenla.fireball();
+})
+
+summonEnemies(goblin, 3);
+
